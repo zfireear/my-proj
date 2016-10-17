1e303ad737d971c163660687b28a2ead6de418ab
zfireear <zfireear@gmail.com>
2016-10-17 15:43:29 +0800

    heroes list display

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,16 +5,31 @@ export class Hero{
     name : string;  
 }
 
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
+
 @Component({
     selector: 'my-app',
     template:  `
        <h1>{{title}}</h1>
-      <h2>{{hero.name}} details!</h2>
-      <div><label>id: </label>{{hero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="hero.name" placeholder="name">
-      </div>
+       <h2>My Heroes</h2>
+        <ul class="heroes">
+          <li *ngFor="let hero of heroes">
+              <span class="badge">{{hero.id}}</span> {{hero.name}}
+        </li>
+        </ul>
+
     `
 })
 
@@ -26,11 +41,15 @@ export class AppComponent {
         this.title = 'Tour of Heroes';
     }
     
-    hero: Hero = {
-      id: 1,
-      name: 'Windstorm'
-    };
+    // hero: Hero = {
+    //   id: 1,
+    //   name: 'Windstorm'
+    // };
 
+    heroes = HEROES;
+    
  }
+
+ 
 
  
