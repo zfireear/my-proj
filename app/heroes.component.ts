1318144f434234f1a02008477d2b965e5c6f86bf
zfireear <zfireear@gmail.com>
2016-10-27 12:54:07 +0800

    add gotoDetail router

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -1,110 +1,43 @@
 import { Component,OnInit } from '@angular/core';
 import {Hero} from "./hero";
 import {HeroService} from './hero.service';
-
-
+import {Router} from '@angular/router';
 
 @Component({
-    selector: 'my-heroes',
-    template:  `
-       
-       <h2>My Heroes</h2>
-        <ul class="heroes">
-          <li *ngFor="let hero of heroes" 
-              [class.selected]="hero === selectedHero"
-              
-              (click)="onSelect(hero)">
-              <span class="badge">{{hero.id}}</span> {{hero.name}}
-        </li>
-        <input #i 
-          (keyup.enter)="values=i.value"
-          (blur)="values=i.value;i.value=''"
-  
-          >
-          <p>{{values}}</p>
-        </ul>
-       
-        <my-hero-detail [hero]="selectedHero"></my-hero-detail>
-
-
-    `,
-    styles: [`
-      .selected {
-        background-color: #CFD8DC !important;
-        color: white;
-      }
-      .heroes {
-        margin: 0 0 2em 0;
-        list-style-type: none;
-        padding: 0;
-        width: 15em;
-      }
-      .heroes li {
-        cursor: pointer;
-        position: relative;
-        left: 0;
-        background-color: #EEE;
-        margin: .5em;
-        padding: .3em 0;
-        height: 1.6em;
-        border-radius: 4px;
-      }
-      .heroes li.selected:hover {
-        background-color: #BBD8DC !important;
-        color: white;
-      }
-      .heroes li:hover {
-        color: #607D8B;
-        background-color: #DDD;
-        left: .1em;
-      }
-      .heroes .text {
-        position: relative;
-        top: -3px;
-      }
-      .heroes .badge {
-        display: inline-block;
-        font-size: small;
-        color: white;
-        padding: 0.8em 0.7em 0 0.7em;
-        background-color: #607D8B;
-        line-height: 1em;
-        position: relative;
-        left: -1px;
-        top: -4px;
-        height: 1.8em;
-        margin-right: .8em;
-        border-radius: 4px 0 0 4px;
-      }
-    `]
-   
-    
+  moduleId: module.id,
+  selector: 'my-heroes',
+  templateUrl: 'heroes.component.html',
+  styleUrls: [ 'heroes.component.css' ]
 })
 
 
 export class HeroesComponent implements OnInit{
+    
+    heroes:Hero[];
+    selectedHero:Hero;
+
     //service construction,create and declare vairable
-    constructor(private heroService: HeroService) { 
-    }
+    constructor(
+      private heroService: HeroService,
+      private router:Router
+      ) {}
     
     //use angular lifecycle OnInit to gain data to aviod constructor do too much work
     ngOnInit() : void{
       this.getHeroes();
     }
 
-    getHeroes(){
+    getHeroes():void{
       //promise then method
       this.heroService.getHeroes().then(heroes=>this.heroes=heroes);
     }
 
-    
-    
-    heroes : Hero[];
-    selectedHero : Hero;
-    values = '';
-
-    onSelect(seHero:Hero){
+    onSelect(seHero:Hero):void{
       this.selectedHero = seHero;
+    }
+
+    gotoDetail():void{
+      this.router.navigate(['/detail',this.selectedHero.id]);
     }
  }
 
