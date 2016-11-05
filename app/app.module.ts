5263523a48ed09c8fa55e493ecd5126a39b6a53d
zfireear <zfireear@gmail.com>
2016-11-05 20:12:01 +0800

    using InMemoryDataService replace mock heroes

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,15 +4,23 @@ import { FormsModule }   from '@angular/forms';
 import { AppComponent }  from './app.component';
 import {HeroDetailComponent} from './hero-detail.component';
 import {HeroesComponent} from './heroes.component';
+
 import {HeroService} from './hero.service';
 import {RouterModule} from '@angular/router';
 import {DashboardComponent} from './dashboard.component';
 import {AppRoutingModule} from './app-routing.module';
 
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+import { HttpModule } from '@angular/http';
+
 @NgModule({
   imports: [ 
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
      ],
   declarations: [ 
--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,15 +1,25 @@
 import { Injectable } from '@angular/core';
 
 import { Hero } from './hero';
+import { Http,Headers } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
 import { HEROES } from './mock-heroes';
+
 
 //defined a service
 @Injectable()
 export class HeroService {
-  //asynchronous Promise
+  
+  constructor(private http:Http){}
+
   getHeroes(): Promise<Hero[]> {
-  return Promise.resolve(HEROES);
-}
+    return this.http.get(this.heroesUrl)
+               .toPromise()
+               .then(response => response.json().data as Hero[])
+               .catch(this.handleError);
+  }
+   // URL to web api
+  private heroesUrl = 'app/heroes';
 
   //get the selected hero
   getHero(id: number): Promise<Hero> {
